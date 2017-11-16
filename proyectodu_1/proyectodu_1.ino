a7f907fdb8598f816da374e68736eb4c8020b7d8
Hinjeniero <davidomfl@gmail.com>
2017-11-16 19:04:09 +0100

    Added a couple of lines

--- proyectodu_1/proyectodu_1.ino
+++ proyectodu_1/proyectodu_1.ino
@@ -7,16 +7,17 @@
 #define D5 4
 #define D6 3
 #define D7 2
-#define DHT11_PIN 6
+#define DHT11_PIN 9
+#define readingDelay 100 //in miliseconds
+int readingsCont = 0;
 bool ok = true;
 dht DHT;
 Sqlite database();
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
-
 void setup() {
   lcd.begin(16, 2);
-  int chk = DHT.read11(DHT11_PIN);
+  lcd.clear();
   checkModules();
   if(ok){
     lcd.print("Initializing...");
@@ -25,24 +26,30 @@ void setup() {
 
 void loop() {
   if(ok){
-    char temp[6]; // Buffer big enough for 5-character float
-    char hum[6]; // Buffer big enough for 5-character float
-    dtostrf(DHT.temperature, 4, 2, temp);
-    dtostrf(DHT.humidity, 4, 2, hum);
+    if(readingsCont == 300){
+      //TODO SAVE IN BD
+      readingsCont = 0;
+    }
+    int chk = DHT.read11(DHT11_PIN);
+    char temp[6]=""; // Buffer big enough for 5-character float
+    char hum[6]=""; // Buffer big enough for 5-character float
+    dtostrf(DHT.temperature, 3, 1, temp);
+    dtostrf(DHT.humidity, 3, 1, hum);
     writeInLcd(temp, hum);
-    delay(100);
+    readingsCont++;
+    delay(readingDelay);
   }
 }
 
 void checkModules(){
-  float n;
+  DHT.read11(DHT11_PIN);
   for(int i=0; i<5; i++){ //5 readings
     if(DHT.temperature < -512){
       ok = false;
       writeInLcd("No DHT sensor!","Check pin");
       break;
     }
-    delay(100);
+    delay(readingDelay);
   }
 }
 
